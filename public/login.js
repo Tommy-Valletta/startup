06a54a3b6943f6a65abd1bc9aa726f6920acdf0a
Tommy Valletta <97928519+Tommy-Valletta@users.noreply.github.com>
2023-11-17 17:22:39 -0700

    HTML, CS, and JS

--- public/login.js
+++ public/login.js
@@ -1,3 +1,15 @@
-function loginAlert() {
-    alert('You have logged in!');
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var stringified = localStorage.getItem(username)
+    if (!stringified) {
+        alert('Invalid username or password');
+    } else {
+        var user = JSON.parse(stringified);
+        if (password == user.password) {
+            location.href = `index.html?user=${username}`;
+        } else {    
+            alert('Invalid username or password');
+        }
+    }
 }
